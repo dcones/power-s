dde53c45e0e73a695892f84b26e5fe0903126563
mingyaang <mmao@ualberta.ca>
2018-11-11 17:16:07 -0700

    powers and transitions

---
+++ Assets/PlayerTransitions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTransitions : MonoBehaviour {
+		private Rigidbody2D rb;
+	public Animator animator;
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+	}
+}
---
+++ Assets/Powers/MobAnimChecks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobAnimChecks : MonoBehaviour {
+	public Animator animator;
+	public MobDeath pc;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetBool ("Death", pc.getHealth() <= 0);
+	}
+}
