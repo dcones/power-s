933caf29ec5831c3badc93af1a8f5af44ea14959
mingyaang <mmao@ualberta.ca>
2018-11-11 22:26:42 -0700

    Ascends

--- Assets/Restart.cs
+++ Assets/Restart.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour {
+	
+	private GameObject uiscreen;
+	public GameObject player;
+	[SerializeField]
+	public GameObject gameOverUI;
 
 	public int health;
 	// Use this for initialization
 	void Start () {
 		health = 1;
+		player = GameObject.FindWithTag ("Player");
 	}
 	void Hurt() {
 		health--;
-		if(health <= 0)
-			Debug.Log("GAME OVER");
-			//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		if (health == 0) {
+			health = -1;
+			gameOverUI.SetActive (true);
+			PolygonCollider2D poly = player.GetComponent<PolygonCollider2D> ();
+			poly.enabled = false;
+				player.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezePositionX;
+		}
+
 	}
 	void OnCollisionEnter2D(Collision2D collision) {
 		Patrol enemy = collision.collider.GetComponent<Patrol>();
