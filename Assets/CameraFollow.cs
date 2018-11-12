d2712d1878f8abe27c948bd365edf1d463ff2d48
dcones <dcones@ualberta.ca>
2018-11-11 22:52:00 -0700

    EnemyShot

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,20 +6,21 @@ public class CameraFollow : MonoBehaviour {
 
 	public GameObject player;
 	public Rigidbody2D body;
-	public float offset;
+	public Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag("Player");
 		body = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
 		this.transform.position = new Vector3(player.transform.position[0], this.transform.position[1], this.transform.position[2]);
-		offset = this.transform.position[0] - player.transform.position[0];
+		//offset = new Vector3(this.transform.position[0] - player.transform.position[0], this.transform.position[1] - player.transform.position[1], this.transform.position[2] - player.transform.position[2]);
+		offset = new Vector3(0.0f, 100.0f, 0);
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float x = player.transform.position[0] - this.transform.position[0] - offset;
-		body.velocity = new Vector2(4*x, 0.0f);
+		Vector3 new_velocity = player.transform.position - this.transform.position + offset;
+		body.velocity = new_velocity*4.0f;
 	}
 }
---
+++ Assets/EnemyFireShot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireShot : MonoBehaviour {
+
+	int time;
+	private GameObject projectile;
+	private GameObject projectileClone;
+	private SpriteRenderer projectileCloneSprite;
+	// Use this for initialization
+	void Start () {
+		time = 0;
+		projectile = GameObject.FindWithTag("EnemyProjectile");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (time >= 180) {
+			projectileClone = Instantiate(projectile, transform.position + new Vector3(-300.0f, 0.0f, 0.0f), transform.rotation);
+			projectileClone.AddComponent<ProjectileBehaviour>();
+			projectileClone.GetComponent<SpriteRenderer>().enabled = true;
+			projectileClone.GetComponent<Rigidbody2D>().velocity = new Vector2(-800.0f, 0.0f);
+			projectileCloneSprite = projectileClone.GetComponent<SpriteRenderer>();
+			projectileCloneSprite.flipX = true;
+			time = 0;
+		}
+		time += 1;
+	}
+}
